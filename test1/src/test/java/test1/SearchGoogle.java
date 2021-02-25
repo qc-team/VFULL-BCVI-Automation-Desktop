430b401216b88d25253ce2fd8ba11b6583f6977a
Shady Ahmed <shadyahmed9719@gmail.com>
2021-02-25 10:07:53 +0200

    nine commit

---
+++ test1/src/test/java/test1/SearchGoogle.java
@@ -0,0 +1,40 @@
+package test1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchGoogle {
+	
+	WebDriver chromeDriver=null;
+	
+	@BeforeTest
+	public void initiateChromeDriver() {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		chromeDriver = new ChromeDriver();
+		chromeDriver.manage().window().maximize();
+
+		chromeDriver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void searchGoogle() {
+		chromeDriver.findElement(By.xpath("//input[@type='text']")).sendKeys("selenium",Keys.ENTER);
+	}
+	
+	@AfterTest
+	public void closeChromeDriver() {
+		// TODO Auto-generated method stub
+		chromeDriver.close();
+		chromeDriver.quit();
+
+	}
+
+}
