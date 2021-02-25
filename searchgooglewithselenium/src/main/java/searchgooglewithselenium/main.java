bb185444911664a503fd43afda3cdb555f7712c7
Shady Ahmed <shadyahmed9719@gmail.com>
2021-02-25 10:06:02 +0200

    seven commit

---
+++ searchgooglewithselenium/src/main/java/searchgooglewithselenium/main.java
@@ -0,0 +1,25 @@
+package searchgooglewithselenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//set system property for chromeDriver
+//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chrome\\chromedriver1.exe");
+//		WebDriver driver=new ChromeDriver();
+//		driver.get("https://www.google.com");
+//		//driver.findElement(By.id("")).sendKeys("Automation with selenium framwork");
+//		WebElement searchElement=driver.findElement(By.name("q"));
+//		searchElement.sendKeys("Automation with selenium");
+//		driver.findElement(By.name("btnI")).click();
+//		
+		System.out.println(System.getProperty("user.dir"));
+
+	}
+
+}
